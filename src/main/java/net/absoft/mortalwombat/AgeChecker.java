1a98b0decf456345d10d0b46db0ac1d289b3f626
Yulia Samorokovskaya <yulia.samorokovskaya@ab-soft.net>
2022-11-30 15:38:48 +0100

    Changes 1

---
+++ src/main/java/net/absoft/mortalwombat/AgeChecker.java
@@ -0,0 +1,9 @@
+package net.absoft.mortalwombat;
+
+public class AgeChecker {
+    private final int MIN_AGE = 18;
+    public boolean canPlayGame (int age)
+    {
+        return age >= MIN_AGE;
+    }
+}
